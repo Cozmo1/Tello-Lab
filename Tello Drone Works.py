d915e9c38f6ff14aa305cd10fb3432b4cb5281bb
Joseph C. Richardson <34896540+Cozmo1@users.noreply.github.com>
2020-05-19 14:02:39 -0400

    Sometimes, most times my poor Tello drone.
    Sometimes, most times my poor Tello drone just won't follow any of its commands. I'm hoping someone can help me figure it out. Sometimes it works, but most of the time it won't. I'm wondering if it's a timeout problem. hmmm?
    Thanks for the help, if anyone can help me.

---
+++ Tello Drone Works.py
@@ -0,0 +1,39 @@
+import threading,socket,sys,time,platform
+from time import sleep
+
+commands=(
+    'takeoff','land','forward 20','back 20','left 20',
+    'right 20','up 20','down 20','cw 360','ccw 360',
+    'flip l','flip r','flip f','flip b','speed 20'
+   )
+
+host = ''
+port = 9000
+locaddr = (host,port)
+address = ('192.168.10.1', 8889)
+asci=('utf-8')
+com=('command')
+
+strap = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+strap.bind(locaddr)
+
+strap.sendto(com.encode(encoding=asci),address)
+sleep(5)
+
+strap.sendto(commands[0].encode(encoding=asci),address)
+sleep(3)
+
+strap.sendto(commands[10].encode(encoding=asci),address)
+sleep(3)
+
+strap.sendto(commands[11].encode(encoding=asci),address)
+sleep(3)
+
+strap.sendto(commands[12].encode(encoding=asci),address)
+sleep(3)
+
+strap.sendto(commands[13].encode(encoding=asci),address)
+sleep(3)
+
+strap.sendto(commands[1].encode(encoding=asci),address)
+strap.close()
